34ff8a97619c71fe332465ac7084b3669022b9f0
marcolarotonda <marco.larotonda.1@gmail.com>
2024-04-28 18:44:23 +0200

    Revert "fix properties"
    This reverts commit 88fb00c4e10aebba39eeec23ac6a6f303495feda.

--- src/main/java/io/github/marcolarotonda/dicerollerclient/Application.java
+++ src/main/java/io/github/marcolarotonda/dicerollerclient/Application.java
@@ -1,16 +1,13 @@
 package io.github.marcolarotonda.dicerollerclient;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(Application.class);
-		springApplication.setWebApplicationType(WebApplicationType.NONE);
-		springApplication.run(args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
